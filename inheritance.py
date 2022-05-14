a52a39b3471be774f3e7eef2ea5f5ae650b5fde6
Prashant <prashant@pop-os.localdomain>
2022-05-14 09:50:55 +0545

    inheritance

--- inheritance.py
+++ inheritance.py
@@ -1,20 +1,33 @@
 # inheritance in python
 
+
+# base class that will be inherited
 class Base:
-    def __inti__(self, a, b):
+
+    # constructor
+    def __init__(self, a, b):
         print("Base class called\n")
         self.a = a
         self.b = b
 
+    # member function
     def display(self):
         print(f'{self.a} {self.b}')
+        
 
-
+# Derived class 
+# inheritance can be applied by writting base class in the brackets
 class Derived(Base):
-    def __inti__(self, x, y):
+    # constrcutor of derived class
+    def __init__(self, x, y):
+        # passing the values to base class's constructor
         super().__init__(x, y)
         print("Derived class called\n")
 
 
+
+# creating object of derived class
 x = Derived(1, 2)
+
+# calling function of base class
 x.display()
